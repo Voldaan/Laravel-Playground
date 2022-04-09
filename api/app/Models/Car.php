4c27be2588e1068a456c1dc450410ee8cba63dc7
Voldaan <autodropcadillac@gmail.com>
2022-04-09 17:13:14 +0200

    Added POST endpoint

--- api/app/Models/Car.php
+++ api/app/Models/Car.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Car extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'brand',
+        'bodytype',
+        'doors',
+        'displacement',
+        'engine_type'
+    ];
 }
