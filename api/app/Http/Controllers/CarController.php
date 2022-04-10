effc312f8ca7879a2d43671475ad28a9ac8f2be5
Voldaan <103193286+Voldaan@users.noreply.github.com>
2022-04-10 09:19:34 +0200

    Feature/crud functions (#1)
    * Added cars migration
    
    * Added POST endpoint
    
    * Added GET all endpoint
    
    * Added PUT endpoint
    
    * Added DELETE endpoint
    
    * Added GET by name endpoint

---
+++ api/app/Http/Controllers/CarController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Car;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Car::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Car::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Car::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $car = Car::find($id);
+        $car->update($request->all());
+        return $car;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Car::destroy($id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function getByName($name)
+    {
+        return Car::where('name', 'like', '%'.$name.'%')->get();
+    }
+}
