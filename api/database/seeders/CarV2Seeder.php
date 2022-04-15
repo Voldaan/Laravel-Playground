e151b80258a642ad4bf6efa12b5d184c24e59038
Voldaan <autodropcadillac@gmail.com>
2022-04-15 09:06:51 +0200

    Added CarV2 seeder

--- api/database/seeders/CarV2Seeder.php
+++ api/database/seeders/CarV2Seeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CarV2Seeder extends Seeder
 {
@@ -14,6 +15,29 @@ class CarV2Seeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('cars_v2')->insert([
+            [
+                'name' => 'Impreza',
+                'brand' => 'Subaru',
+                'bodytype' => 'Sedan',
+                'doors' => 4,
+                'engine_id' => 1
+            ],
+            [
+                'name' => 'Toyota',
+                'brand' => 'Supra',
+                'bodytype' => 'Coupe',
+                'doors' => 2,
+                'engine_id' => 2
+            ],
+            [
+                'name' => 'Civic Type-R',
+                'brand' => 'Honda',
+                'bodytype' => 'Hatchback',
+                'doors' => 2,
+                'engine_id' => 3
+            ]
+        ]
+        );
     }
 }
